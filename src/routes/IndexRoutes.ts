49bbb580ebb2956b978d05351294acd0b0100284
DARIKA.KIM <DARIKA.KIM@PNC.LOCAL>
2026-05-28 19:55:54 +0700

    rika fix code category

--- src/routes/IndexRoutes.ts
+++ src/routes/IndexRoutes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import authRoutes from './AuthRoutes';
-import budgetRoutes from './BudgetRoutes';
 import categoryRoutes from './CategoryRoutes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/budgets', budgetRoutes);
 router.use('/categories', categoryRoutes);
 
 export default router;
